c0e7e73e12140347c44d61c2502a942514a6dc2a
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-25 14:37:35 -0300

    feat: Added EmailServiceException

---
+++ src/main/java/acc/project/email_service/domain/exceptions/EmailServiceException.java
@@ -0,0 +1,11 @@
+package acc.project.email_service.domain.exceptions;
+
+public class EmailServiceException extends RuntimeException {
+  public EmailServiceException(String message){
+    super(message);
+  }
+
+  public EmailServiceException(String message, Throwable cause){
+    super(message, cause);
+  }
+}
